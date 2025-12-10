e293f58e145346ae41ca38ceb9f97c4f814e19eb
Saeed <saeed_azarafrooz@yahoo.com>
2025-12-10 15:38:50 +0330

    feat: implement CartItem component and add addToCart method to CartContext and use it in relative components

--- src/components/cart-dropdown/Cart-Dropdown.jsx
+++ src/components/cart-dropdown/Cart-Dropdown.jsx
@@ -1,11 +1,17 @@
 import './cart-dropdown.scss'
 import Button from "../button/Button"
+import CartItem from '../cart-item/Cart-Item'
+import { useContext } from 'react';
+import { CartContext } from '../contexts/cart.context';
 
 const CartDropdown = () => {
+        const { cartItems } = useContext(CartContext);
+    
     return (
       
             <div className="cart-dropdown-container">
                 <div className="cart-items">
+                   {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)} 
                 </div>
                 <Button>GO TO CHECKOUT</Button>
             </div>
--- src/components/cart-icon/Cart-Icon.jsx
+++ src/components/cart-icon/Cart-Icon.jsx
@@ -5,14 +5,14 @@ import { CartContext } from '../../components/contexts/cart.context';
 import { useContext } from 'react';
 
 function CartIcon() {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const { isCartOpen, setIsCartOpen ,cartItems,cartCount} = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div onClick={toggleIsCartOpen} className='cart-icon-container'>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>10</span>
+        <span className='item-count'>{cartCount}</span>
     </div>
   )
 }

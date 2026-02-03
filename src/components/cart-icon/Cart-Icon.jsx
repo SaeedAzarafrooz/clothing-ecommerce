01487f4e81bf0cae3ca00760764592fe854d6d52
Saeed <saeed_azarafrooz@yahoo.com>
2026-02-03 19:44:35 +0330

    feat: add useref hook to CartIcon component

--- src/components/cart-icon/Cart-Icon.jsx
+++ src/components/cart-icon/Cart-Icon.jsx
@@ -6,18 +6,19 @@ import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selecto
 import { setIsCartOpen } from '../../store/cart/cart.action';
 
 
-function CartIcon() {
+function CartIcon({ iconRef }) {
   const dispatch = useDispatch();
   const isCartOpen = useSelector(selectIsCartOpen);
   const cartCount = useSelector(selectCartCount);
 
-  const toggleIsCartOpen = () => {
+  const toggleIsCartOpen = (e) => {
+    e.stopPropagation();
     dispatch(setIsCartOpen(!isCartOpen));
   };
   return (
-    <div onClick={toggleIsCartOpen} className='group relative w-11 h-11 flex justify-center items-center  cursor-pointer hover:bg-slate-300 rounded-md transition-all duration-300 ease-in-out'>
-      <ShoppingIcon className='w-7 h-7 block group-hover:hidden' />
-      <ShoppingIconHover className='w-7 h-7 hidden group-hover:block' />
+    <div ref={iconRef} onClick={toggleIsCartOpen} className='group relative w-11 h-11 flex justify-center items-center  cursor-pointer hover:bg-slate-300 rounded-md transition-all duration-300 ease-in-out'>
+      <ShoppingIcon className='absolute top-2 left-2 w-7 h-7 opacity-100 group-hover:opacity-0 transition-opacity duration-300 ease-in-out pointer-events-none' />
+      <ShoppingIconHover className='absolute top-2 left-2 w-7 h-7 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none' />
       <span className='absolute top-5 text-xs font-SG-light'>{cartCount}</span>
     </div>
   )

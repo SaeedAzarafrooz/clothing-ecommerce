57a3b9f05586e869c4adb3133a3e3decb3979e2d
Saeed <saeed_azarafrooz@yahoo.com>
2026-02-03 19:55:14 +0330

    feat: add ref to Navigation for close dropDowns

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -2,7 +2,7 @@
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
 
 import { Link, Outlet, useLocation } from "react-router-dom"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import CartIcon from "../../components/cart-icon/Cart-Icon"
 import CartDropdown from "../../components/cart-dropdown/Cart-Dropdown"
 import { useDispatch, useSelector } from 'react-redux'
@@ -13,6 +13,7 @@ import ProfileIcon from '../../components/profile-icon/Profile-Icon'
 import ProfileDropdown from '../../components/profile-dropdown/Profile-Dropdown'
 import shopIcon from "../../assets/shopIcon3.svg";
 import shopIconHover from "../../assets/shopIcon-hover5.svg";
+import SearchBox from "../../components/search-box/SearchBox"
 
 const Navigation = () => {
     const location = useLocation();
@@ -21,7 +22,9 @@ const Navigation = () => {
     const isCartOpen = useSelector(selectIsCartOpen);
     const isProfileOpen = useSelector(selectIsProfileOpen);
     const currentUser = useSelector(selectCurrentUser);
-    console.log('currentUser:', currentUser);
+    // console.log('currentUser:', currentUser);
+    const cartIconRef = useRef(null);
+    const profileIconRef = useRef(null);
 
 
     useEffect(() => {
@@ -37,28 +40,31 @@ const Navigation = () => {
                 <Link className="h-full w-16 flex justify-center items-center" to="/">
                     <CrwnLogo />
                 </Link>
+                <SearchBox/>
                 <div className="flex items-center gap-2 ">
-                    <Link to="/shop" className='group flex items-center justify-center rounded-md cursor-pointer px-2 py-1  gap-2 h-11 hover:bg-slate-300 transition-all duration-300 ease-in-out'>
+                    <Link to="/shop" className='group w-26 flex items-center justify-center rounded-md cursor-pointer px-2 py-1 gap-2  h-11 hover:bg-slate-300 transition-all duration-300 ease-in-out'>
+                       <div className="group relative w-7 h-7 flex-shrink-0">
                         <img
                             src={shopIcon}
                             alt="shop"
-                            className="w-7 h-7 block group-hover:hidden"
-                        />
+                            className="absolute inset-0 w-7 h-7 opacity-100 group-hover:opacity-0 transition-opacity duration-300 ease-in-out pointer-events-none"
+                            />
 
                         <img
                             src={shopIconHover}
                             alt="shop-hover"
-                            className="w-7 h-7 hidden group-hover:block"
-                        />
+                            className="absolute inset-0 w-7 h-7 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none"
+                            />
+                            </div>
                         <span className="font-SG" >SHOP</span>
                     </Link>
 
-                    <CartIcon />
-                    <ProfileIcon />
+                    <CartIcon iconRef={cartIconRef}/>
+                    <ProfileIcon iconRef={profileIconRef}/>
 
                 </div>
-                {isCartOpen && <CartDropdown />}
-                {isProfileOpen && <ProfileDropdown currentUser={currentUser} />}
+                {isCartOpen && <CartDropdown iconRef={cartIconRef}/>}
+                {isProfileOpen && <ProfileDropdown currentUser={currentUser} iconRef={profileIconRef}/>}
             </div>
             <Outlet />
         </>

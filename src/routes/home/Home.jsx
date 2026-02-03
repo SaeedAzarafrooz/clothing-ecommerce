b77f012978632414c773d06ca21997cf91d283b7
Saeed <saeed_azarafrooz@yahoo.com>
2026-02-03 19:54:01 +0330

    feat: add Slider and Carousel components to Home page

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
+import Carousel from "../../components/Carousel";
 import Directory from "../../components/directory/directory";
+import Slider from "../../components/Slider";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 
 function Home() {
+      const categoriesMap = useSelector(selectCategoriesMap);
+
     const categories = [
         { id: 1, title: 'Hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png' },
         { id: 2, title: 'Jackets', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png' },
@@ -11,8 +17,16 @@ function Home() {
             id: 5, title: 'Mens', imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         },
     ];
+    console.log('test:',categoriesMap);
+    
     return (<>
-        <Directory categories={categories} />
+        <Slider />
+        <Carousel />
+        <Directory categories={categoriesMap} />
+
+        {/* footer component */}
+        {/* add brands to product data */}
+
     </>
     );
 }

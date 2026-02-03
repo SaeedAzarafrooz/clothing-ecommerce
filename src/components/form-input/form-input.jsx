fb7aec4f8ea6cb39416c988b2422e20f582c363c
Saeed <saeed_azarafrooz@yahoo.com>
2026-02-03 19:52:36 +0330

    feat: upgrade FormInput component

--- src/components/form-input/form-input.jsx
+++ src/components/form-input/form-input.jsx
@@ -1,11 +1,10 @@
 // import './form-input.scss'
 
-const FormInput = ({ label, ...otherProps }) => {
+const FormInput = ({ label, className, ...otherProps }) => {
     return (<>
         <div className="relative my-11 group">
             <input
-                className={`peer block w-full border-b border-gray-500 bg-white py-2.5 pr-2.5 pl-1.5 my-6
-                        text-lg text-gray-500 focus:outline-none focus:border-black
+                className={`${className} peer block w-full border-b border-gray-500 bg-white py-2.5 pr-2.5 pl-1.5 text-lg text-gray-500 focus:outline-none focus:border-black
                         ${otherProps.type === 'password' ? 'tracking-[0.3em]' : ''}
                         ${otherProps.value && String(otherProps.value).length ? 'bg-slate-200 ' : ''}`}
                 placeholder=" " // Required for the peer-placeholder-shown logic
@@ -13,11 +12,12 @@ const FormInput = ({ label, ...otherProps }) => {
             />
             {label && (
                 <label
-                    className={`
-            absolute left-1.5 top-2.5 transition-all duration-300 pointer-events-none text-gray-500 text-base
-            peer-focus:-top-5 peer-focus:text-sm peer-focus:text-black
-            ${console.log('otherProps:',otherProps) }
-            ${otherProps.value && String(otherProps.value).length ? '-top-5 text-sm text-black ' : ''}`}>
+                    className=' absolute left-1.5 -top-5 text-sm text-gray-500 
+                                transition-all duration-300 pointer-events-none
+                                peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-base
+                                peer-placeholder-shown:text-gray-500 peer-focus:-top-5
+                                peer-focus:text-sm peer-focus:text-black
+            '>
                     {label}
                 </label>
             )}
